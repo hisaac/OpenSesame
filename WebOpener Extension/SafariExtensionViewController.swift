b32b28b6a4c75a9c6bf531d1852301199949f23f
Isaac Halvorson <hello@hisaac.net>
2020-11-30 08:40:41 -0600

    Initial Commit

---
+++ WebOpener Extension/SafariExtensionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SafariExtensionViewController.swift
+//  WebOpener Extension
+//
+//  Created by Isaac Halvorson on 11/30/20.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared: SafariExtensionViewController = {
+        let shared = SafariExtensionViewController()
+        shared.preferredContentSize = NSSize(width:320, height:240)
+        return shared
+    }()
+
+}
---
+++ WebOpener/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  WebOpener
+//
+//  Created by Isaac Halvorson on 11/30/20.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
